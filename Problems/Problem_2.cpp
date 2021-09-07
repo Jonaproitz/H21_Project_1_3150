66378c87ec9bd03071e291d6283af7c8c0a4dc15
jonaproitz <jonaproitz@gmail.com>
2021-09-07 12:55:42 +0200

    Created main function solving exact solution for an array x of length N, and writing result to file as Nx2 matrix

--- Problems/Problem_2.cpp
+++ Problems/Problem_2.cpp
@@ -6,6 +6,12 @@ arma::vec u(arma::vec x){
     return val;
 }
 
-int init(){
+int main(int argc, char* argv[]){
+    int N = atoi(argv[1]);
+    arma::vec x = arma::linspace(0, 1, N);
+    arma::mat A = arma::mat(N, 2);
+    A.col(0) = x;
+    A.col(1) = u(x);
+    A.save("matrix.bin");
     return 0;
 }
