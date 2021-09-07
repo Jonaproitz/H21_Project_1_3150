9102cc1c1c31bd2c1bd70d7c79b083fd5f872239
jonaproitz <jonaproitz@gmail.com>
2021-09-07 14:41:23 +0200

    Solved task in an excidingly more tedious way

--- Problems/Problem_2.cpp
+++ Problems/Problem_2.cpp
@@ -1,5 +1,6 @@
 
 #include <armadillo>
+#include <iomanip>
 
 arma::vec u(arma::vec x){
     arma::vec val = 1 - (1 - exp(-10)) * x - exp(-10*x);
@@ -7,11 +8,36 @@ arma::vec u(arma::vec x){
 }
 
 int main(int argc, char* argv[]){
+    // Create x array and Nx2 Matrix to store it and u(x)
     int N = atoi(argv[1]);
     arma::vec x = arma::linspace(0, 1, N);
     arma::mat A = arma::mat(N, 2);
+
+    // Set columns of A matrix to x and u(x)
     A.col(0) = x;
     A.col(1) = u(x);
-    A.save("matrix.bin");
+    
+    // Set filename
+    std::string filename = "matrix.txt";
+
+    // Create and open the file
+    std::ofstream ofile;
+    ofile.open(filename);
+
+    // Set width and precision
+    int width = 12;
+    int prec = 4;
+
+    // Loop over rows
+    for (int i = 0; i < N; i++)
+    {
+        // Write line with current row in A
+        ofile << std::setw(width) << std::setprecision(prec) << std::scientific << A(i, 0)
+              << std::setw(width) << std::setprecision(prec) << std::scientific << A(i, 1)
+              << std::endl;
+    }
+
+    // Close output file
+    ofile.close();
     return 0;
 }
--- Problems/Problem_2.py
+++ Problems/Problem_2.py
@@ -1,8 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import pyarma as pa
 
 def p(filename):
-	pass
+	A = np.loadtxt(filename)
+	A = np.transpose(A)
+	
+	plt.figure()
+	plt.title("")
 
+	plt.plot(A[0], A[1])
+
+	plt.show()
+
+p("matrix.txt")
 
