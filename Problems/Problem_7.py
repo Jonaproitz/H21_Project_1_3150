17ffadd3a3ac58ada850d9dd5164250f2fb90a7f
jonaproitz <jonaproitz@gmail.com>
2021-09-09 13:53:29 +0200

    Made a plotting script

--- Problems/Problem_7.py
+++ Problems/Problem_7.py
@@ -1,6 +1,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import os    
 
-def p(filename):
-    pass
+N = [10, 100, 1000]
+plt.figure()
+plt.title("", fontsize=15)
+for N in N:
+    os.system(f"~/Documents/Sem_7/FYS3150/Project_1/Problems/Problem_7.exe {N}")
+    x, v = np.transpose(np.loadtxt("approx.txt"))
+    plt.plot(x, v, label=f"{N}")
 
+A = np.loadtxt("matrix.txt")
+A_T = np.transpose(A)
+
+plt.plot(A_T[0], A_T[1], label="$u(x)$", linestyle="--")
+
+plt.grid()
+plt.legend()
+plt.show()
