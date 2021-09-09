1cb321e702ec29b9e32277d2de315ad920ac00b9
jonaproitz <jonaproitz@gmail.com>
2021-09-09 10:57:36 +0200

    Implemented writing to file

--- Problems/Problem_7.cpp
+++ Problems/Problem_7.cpp
@@ -36,6 +36,8 @@ int main(int argc, char* argv[]){
     arma::vec g_tilde = arma::vec(n);
     arma::vec v = arma::vec(n);
 
+    arma::vec v_star_1 = arma::vec(1).fill(v_0), v_star_m = arma::vec(1).fill(v_m);
+
     // Running over steps in the algorithm
     b_tilde(0) = b(0);
     g_tilde(0) = g(0);
@@ -44,12 +46,34 @@ int main(int argc, char* argv[]){
         b_tilde(i) = b(i) - fac*c(i-1);
         g_tilde(i) = g(i) - fac*g_tilde(i-1);
     }
-    //g_tilde.print();
+
     v(n-1) = g_tilde(n-1)/b_tilde(n-1);
     for (int i = n-2; i >= 0; i--){
         v(i) = (g_tilde(i) - c(i)*v(i+1)) / b_tilde(i);
     }
-    v.print();
+    
+    // Implement solution into the complete solution
+    arma::vec v_star_merge = join_cols(v_star_1, v);
+    arma::vec v_star = join_cols(v_star_merge, v_star_m);
 
+    // Set filename and open file
+    std::string filename = "approx.txt";
+    std::ofstream ofile;
+    ofile.open(filename);
+
+    // Set width and precision
+    int width = 18;
+    int prec = 10;
+
+    // Loop over rows
+    for (int i = 0; i < m; i++)
+    {
+        // Write relevant values to file
+        ofile << std::setw(width) << std::setprecision(prec) << std::scientific << x(i)
+              << std::setw(width) << std::setprecision(prec) << std::scientific << v_star(i)
+              << std::endl;
+    }
+
+    ofile.close();
     return 0;
 }
