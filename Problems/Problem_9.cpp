fe7a09e562d0364c5ae85f83c96af120b1a6e706
jonaproitz <jonaproitz@gmail.com>
2021-09-12 01:20:02 +0200

    Implemented special algorithm

--- Problems/Problem_9.cpp
+++ Problems/Problem_9.cpp
@@ -11,8 +11,12 @@ int main(int argc,char* argv[]){
     int m = atoi(argv[1]);
     int n = m - 2;
 
-    double dt = 1.0 / (m-1);
-    arma::vec g = func(arma::linspace(dt, 1-dt, n));
+    double h = 1.0 / (m-1);
+    arma::vec x_star = arma::linspace(h, 1-h, n);
+    arma::vec g = func(x_star) * h * h;
+
+    arma::vec x_0 = arma::vec(1).fill(0.), x_m = arma::vec(1).fill(1.);
+    arma::vec x = join_cols(x_0, x_star, x_m);
 
     arma::vec b_tilde = arma::vec(n);
     b_tilde(0) = 2;
@@ -22,7 +26,37 @@ int main(int argc,char* argv[]){
     
     arma::vec v = arma::vec(n);
 
+    for (int i = 1; i < n; i++){
+        double fac = 1 / b_tilde(i - 1);
+        b_tilde(i) = 2 - fac;
+        g_tilde(i) = g(i) + fac * g_tilde(i - 1);
+    }
 
+    v(n-1) = g_tilde(n-1) / b_tilde(n-1);
+    for (int i = n-2; i >= 0; i--){
+        v(i) = (g_tilde(i) + v(i+1)) / b_tilde(i);
+    }
+    arma::vec v_0 = arma::vec(1).fill(0.), v_m = arma::vec(1).fill(0.);
+    arma::vec v_star = join_cols(v_0, v, v_m);
 
+    // Set filename and open file
+    std::string filename = "new_approx.txt";
+    std::ofstream ofile;
+    ofile.open(filename);
+
+    // Set width and precision
+    int width = 18;
+    int prec = 10;
+
+    // Loop over rows
+    for (int i = 0; i < m; i++)
+    {
+        // Write relevant values to file
+        ofile << std::setw(width) << std::setprecision(prec) << std::scientific << x(i)
+              << std::setw(width) << std::setprecision(prec) << std::scientific << v_star(i)
+              << std::endl;
+    }
+
+    ofile.close();
     return 0;
 }
